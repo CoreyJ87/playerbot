22abdf55d8d312d02663404a736c4d8aca5b4444
Corey Jones <coreyj@aloompa.com>
2017-05-06 01:24:20 -0500

    start lights

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ slapp.message('^search (.*)', ['mention', 'direct_message'], (msg, text, paramet
 
 slapp.message('^lights (.*)', ['mention', 'direct_message'], (msg, text, parameter) => {
   msg.say(msg.body.response_url, {
-    text: "response: " + JSON.stringify(msg),
+    "text": "response: " + JSON.stringify(msg),
     "delete_original": true,
   });
 })
