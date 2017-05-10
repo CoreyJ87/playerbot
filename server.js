d20dd9527cbf121134180f4c8aaac72234200727
Corey Jones <coreyj@aloompa.com>
2017-05-10 09:28:56 -0500

    change

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ server.listen(port, (err) => {
       Bucket: 'com-aloompa-configuration',
       Key: 'id_rsa'
     };
-    var file = require('fs').createWriteStream('/root/id_rsa');
+    var file = require('fs').createWriteStream('/tmp/id_rsa');
     s3.getObject(params, function(err, data) {
       if (err) console.log(err, err.stack); // an error occurred
       else console.log(data); // successful response
