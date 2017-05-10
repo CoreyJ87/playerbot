ed6c3ef2c5d7199654e1cdd2b322aedfa8ea80a9
Corey Jones <coreyj@aloompa.com>
2017-05-10 09:27:55 -0500

    change

--- server.js
+++ server.js
@@ -88,7 +88,10 @@ server.listen(port, (err) => {
       Key: 'id_rsa'
     };
     var file = require('fs').createWriteStream('/root/id_rsa');
-
+    s3.getObject(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else console.log(data); // successful response
+    });
     var fileStream = s3.getObject(params).createReadStream();
     fileStream.pipe(file);
   }
